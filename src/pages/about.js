846b3f0d753d0c7df36528e3d294a841597ba5e8
Lykos45 <swnuno05@yahoo.com>
2021-05-04 19:08:22 -0500

    added pages; made header changes

---
+++ src/pages/about.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Layout from "../components/layout"
+import Seo from "../components/seo"
+
+const About = () => {
+    return (
+        <Layout>
+            <Seo title="About" />
+            <h1>About</h1>
+            <p>My name is Carlos and this page is about me!</p>
+        </Layout>
+    )
+}
+
+export default About
---
+++ src/pages/blog.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Layout from "../components/layout"
+import Seo from "../components/seo"
+
+const Blog = () => {
+    return (
+        <Layout>
+            <Seo title="Blog" />
+            <h1>Blog</h1>
+            <p>Leave a comment.</p>
+        </Layout>
+    )
+}
+
+export default Blog
